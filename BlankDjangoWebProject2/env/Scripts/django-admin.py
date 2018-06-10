dc7c3ec1b56c55552a9dee8ab12a46d02aab9cce
Hung-Jie Yang <taichishadow@gmail.com>
2018-06-10 09:49:30 +0800

    加入專案檔案。

---
+++ BlankDjangoWebProject2/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\程式碼\BlankDjangoWebProject2\BlankDjangoWebProject2\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
