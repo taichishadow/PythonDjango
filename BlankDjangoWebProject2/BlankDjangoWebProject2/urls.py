4e38941834031dc8d871a9b474ce67341f10dde4
Hung-Jie Yang <taichishadow@gmail.com>
2018-06-10 10:31:08 +0800

    crate hello world

--- BlankDjangoWebProject2/BlankDjangoWebProject2/urls.py
+++ BlankDjangoWebProject2/BlankDjangoWebProject2/urls.py
@@ -3,6 +3,7 @@ Definition of urls for BlankDjangoWebProject2.
 """
 
 from django.conf.urls import include, url
+import HelloDjangoApp.views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -18,4 +19,7 @@ urlpatterns = [
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', HelloDjangoApp.views.index, name='index'),
+    url(r'^home$', HelloDjangoApp.views.index, name='home'),
 ]
