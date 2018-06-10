28fdf5444e600677ef439284b225e4cb94ab6038
Hung-Jie Yang <taichishadow@gmail.com>
2018-06-10 11:18:51 +0800

    done the step 2

--- BlankDjangoWebProject2/HelloDjangoApp/views.py
+++ BlankDjangoWebProject2/HelloDjangoApp/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello world")
+    now = datetime.now()
+    
+    return render(
+        request, 
+         "HelloDjangoApp/index.html",  # Relative path from the 'templates' folder to the template file
+        {
+            'title': "this is a test project",
+            'message': "Hello Django! on ",
+            'content': now.strftime("%A, %d %B, %Y at %X"),
+        }
+    )
