c3140ca652bf8196fab4bd5e5fbbf365425ab1ed
Hung-Jie Yang <taichishadow@gmail.com>
2018-06-18 15:32:56 +0800

    add models

--- BlankDjangoWebProject2/HelloDjangoApp/models.py
+++ BlankDjangoWebProject2/HelloDjangoApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Role(models.Model):
+    name = models.CharField(max_length=20)
+
+class User(models.Model):
+    account = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50, blank=True)
+    photo = models.CharField(max_length=20, blank=True)
+    role = models.ForeignKey(Role);
